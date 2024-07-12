6526be7b4cdc1c152e7cbe55dec0d24011ba97f1
Guilherme Lima Gonçalves <guligon90@gmail.com>
2024-07-12 05:53:42 -0300

    fix: integrate csv processing into project entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 "use strict";
 
+import { join } from "node:path";
 import registerModuleAliases from "./modules";
 
 registerModuleAliases();
 
-import { Logger } from "./sdk/logging";
+import { parseArguments } from "./arguments";
+import { Logging } from "./sdk/logging";
+import { processCsvFile } from "./csv/processing";
 
-const logger = Logger(`fscc-kronoos@${__filename}`);
+function main(logger: Logging) {
+    const dataFile = (fileName: string) => join(__dirname, `../../${fileName}`);
+
+    const {
+        values: { csvfile },
+    } = parseArguments();
+
+    if (typeof csvfile === "string") processCsvFile(logger, dataFile(csvfile));
+}
 
 (() => {
-    logger.info("Initiating CSV Validator...");
+    const logger = new Logging(`fscc-kronoos`);
+
+    logger.info("Initiating CSV Validator...", main.name);
+    main(logger);
 })();
