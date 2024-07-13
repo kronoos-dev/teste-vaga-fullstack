4c973f2fddbbc28eb04fb1d83a207808d86f2077
Guilherme Lima Gonçalves <guligon90@gmail.com>
2024-07-13 09:47:12 -0300

    chore: rectify eslint config module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,85 +13,92 @@ const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: [
-        "**/build",
-        "**/dist",
-        "**/node_modules",
-        "**/package-lock.json",
-        "**/tsconfig.json",
-        "**/yarn.lock",
-    ],
-}, ...compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"), {
-    plugins: {
-        import: fixupPluginRules(_import),
-        "@typescript-eslint": typescriptEslint,
+export default [
+    {
+        ignores: ["**/build", "**/dist", "**/node_modules", "**/package-lock.json", "**/tsconfig.json", "**/yarn.lock"],
     },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+    ...compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"),
+    {
+        plugins: {
+            import: fixupPluginRules(_import),
+            "@typescript-eslint": typescriptEslint,
         },
 
-        parser: tsParser,
-        ecmaVersion: 2020,
-        sourceType: "module",
-
-        parserOptions: {
-            tsconfigRootDir: "./",
-        },
-    },
-
-    settings: {
-        "import/extensions": [".js", ".ts"],
-
-        "import/parsers": {
-            "@typescript-eslint/parser": [".ts"],
-        },
-
-        "import/resolver": {
-            node: {
-                extensions: [".js", ".ts"],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
             },
 
-            typescript: {},
+            parser: tsParser,
+            ecmaVersion: 2020,
+            sourceType: "module",
+
+            parserOptions: {
+                tsconfigRootDir: "./",
+            },
+        },
+
+        settings: {
+            "import/extensions": [".js", ".ts"],
+
+            "import/parsers": {
+                "@typescript-eslint/parser": [".ts"],
+            },
+
+            "import/resolver": {
+                node: {
+                    extensions: [".js", ".ts"],
+                },
+
+                typescript: {},
+            },
+        },
+
+        rules: {
+            "import/extensions": [
+                "error",
+                "ignorePackages",
+                {
+                    js: "never",
+                    ts: "never",
+                },
+            ],
+
+            "no-console": [
+                "error",
+                {
+                    allow: ["info", "warn", "error"],
+                },
+            ],
+
+            "multiline-ternary": 0,
+            "no-unused-vars": "off",
+            "no-shadow": 0,
+            "@typescript-eslint/no-unused-vars": "error",
+            "no-useless-constructor": "off",
+            "@typescript-eslint/no-useless-constructor": "error",
+            "@typescript-eslint/no-empty-function": "off",
+            "@typescript-eslint/no-empty-interface": "off",
+            "@typescript-eslint/no-namespace": "off",
+            "@typescript-eslint/no-non-null-assertion": "off",
+
+            "max-len": [
+                "warn",
+                {
+                    code: 120,
+                    tabWidth: 2,
+                    comments: 120,
+                    ignoreComments: false,
+                    ignoreTrailingComments: true,
+                    ignoreUrls: true,
+                    ignoreStrings: true,
+                    ignoreTemplateLiterals: true,
+                    ignoreRegExpLiterals: true,
+                },
+            ],
         },
     },
-
-    rules: {
-        "import/extensions": ["error", "ignorePackages", {
-            js: "never",
-            ts: "never",
-        }],
-
-        "no-console": ["error", {
-            allow: ["info", "warn", "error"],
-        }],
-
-        "multiline-ternary": 0,
-        "no-unused-vars": "off",
-        "no-shadow": 0,
-        "@typescript-eslint/no-unused-vars": "error",
-        "no-useless-constructor": "off",
-        "@typescript-eslint/no-useless-constructor": "error",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-empty-interface": "off",
-        "@typescript-eslint/no-namespace": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-
-        "max-len": ["warn", {
-            code: 120,
-            tabWidth: 2,
-            comments: 120,
-            ignoreComments: false,
-            ignoreTrailingComments: true,
-            ignoreUrls: true,
-            ignoreStrings: true,
-            ignoreTemplateLiterals: true,
-            ignoreRegExpLiterals: true,
-        }],
-    },
-}];
+];
