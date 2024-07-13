87ba115b211938b5e606c3e7b5d9e5478b8220b0
Guilherme Lima Gonçalves <guligon90@gmail.com>
2024-07-13 09:46:02 -0300

    fix: rectify app entrypoint due to code refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,28 @@
 "use strict";
 
-import { join } from "node:path";
 import registerModuleAliases from "./modules";
 
 registerModuleAliases();
 
-import { parseArguments } from "./arguments";
-import { Logging } from "./sdk/logging";
-import { processCsvFile } from "./csv/processing";
+import config, { buildEnvMessage, validateAppConfig } from "./app/config";
+import { declareProcessListeners } from "@app/process";
+import { Logging } from "@sdk/logging";
+import { main } from "@src/app";
+import { ValidationError } from "./sdk/exceptions";
 
-function main(logger: Logging) {
-    const buildFilePath = (fileName: string) => join(__dirname, `../../${fileName}`);
+const logger = new Logging(`fscc-kronoos`);
 
-    const {
-        values: { inputcsv, outputcsv, reportcsv },
-    } = parseArguments();
-
-    const csvPaths = {
-        input: buildFilePath(inputcsv as string),
-        output: buildFilePath(outputcsv as string),
-        report: buildFilePath(reportcsv as string),
-    };
-
-    processCsvFile(logger, csvPaths);
-}
+declareProcessListeners(logger);
 
 (() => {
-    const logger = new Logging(`fscc-kronoos`);
+    const validationErrors = validateAppConfig(config);
 
+    if (validationErrors.length) {
+        throw new ValidationError(validationErrors);
+    }
+
+    logger.info(buildEnvMessage(config), main.name);
     logger.info("Initiating CSV Validator...", main.name);
+
     main(logger);
 })();
