6c50c5912c8d48c480c4b25f3a8ab4fbab2b7b98
Natan moreira <natan-moreira1@hotmail.com>
2024-07-15 09:01:26 -0300

    feat: create pipelines to process data

---
+++ src/const/csv-schema.ts
@@ -0,0 +1,9 @@
+export const csvOutSchema = [
+  { id: "clientCode", title: "clientCode" },
+  { id: "clientName", title: "clientName" },
+  { id: "clientDocument", title: "clientDocument" },
+  { id: "isVlTotalCorrect", title: "isVlTotalCorrect" },
+  { id: "vlTotal", title: "vlTotal" },
+  { id: "vlPresta", title: "vlPresta" },
+  { id: "qtPrestacoes", title: "qtPrestacoes" },
+];
